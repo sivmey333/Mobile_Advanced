5d3f04f800397f2f5c831a722bf95ab8f47967b4
test <ppc@PPCs-MacBook-Pro.local>
2025-05-03 15:17:51 +0700

    my new source code

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,21 +1,48 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Flutter Demo', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
+//         backgroundColor: Colors.red,
+//       ),
+//       body: Center(
+//         child: Text(
+//           'Hello World',style: TextStyle(fontSize: 20),
+//         ),
+//       ),
+
+//     );
+//   }
+// }
+class Home extends StatelessWidget {
+  final String title;
+  const Home({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Demo', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
-        backgroundColor: Colors.red,
+        title: Text(title),
+        backgroundColor: Theme.of(context).colorScheme.primary,
       ),
       body: Center(
-        child: Text(
-          'Hello World',style: TextStyle(fontSize: 20),
+        child: Container(
+          color: Theme.of(context).colorScheme.secondary,
+          child: Text(
+            'Hello Geeks',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
         ),
       ),
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.arrow_circle_up),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const appName = 'GeeksForGeeks';
     return MaterialApp(
+      title: appName,
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+              backgroundColor: Colors.cyan.shade500),
+          appBarTheme: AppBarTheme(foregroundColor: Colors.white),
+          brightness: Brightness.light,
+          primaryColor: Colors.cyanAccent,
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.deepPurple, secondary: Colors.purple),
+          useMaterial3: true,
+          fontFamily: 'Angkor',
+          textTheme: const TextTheme(
+              displayLarge:
+                  TextStyle(fontSize: 70.0, fontWeight: FontWeight.bold),
+              titleLarge:
+                  TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+              bodyMedium: TextStyle(fontSize: 14.0, fontFamily: 'Angkor'))),
+      home: const Home(
+        title: appName,
       ),
-      home: const HomePage(),
     );
   }
 }
-
-
