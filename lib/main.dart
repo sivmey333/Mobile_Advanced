bfd343657ccd6005d3d0d13b6c805f3df0842388
test <ppc@PPCs-MacBook-Pro.local>
2025-05-04 08:02:21 +0700

    about skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:skeleton_text/skeleton_text.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,17 +19,56 @@ class MyApp extends StatelessWidget {
           title: Text('GeeksForGeeks'),
           backgroundColor: Colors.green,
         ),
-        body: Center(
-          child: SizedBox(
-            width: 200,
-            height: 150,
-            child: AutoSizeText(
-              'Hello Geeks!. we will break this line into 3 lines!! I love you so much. Did you know?',
-              style: Theme.of(context).textTheme.titleLarge,
-              maxLines: 3,
-            ),
-          ),
-        ),
+        body: ListView.builder(
+            scrollDirection: Axis.vertical,
+            physics: BouncingScrollPhysics(),
+            itemCount: 5,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 30,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.purpleAccent,
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      SkeletonAnimation(
+                        child: Container(
+                          width: 70,
+                          height: 70,
+                          decoration: BoxDecoration(color: Colors.greenAccent),
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(left: 15, bottom: 5),
+                            child: SkeletonAnimation(
+                              child: Container(
+                                height: 15,
+                                width: MediaQuery.of(context).size.width * 0.6,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.pink,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              );
+            }),
       ),
     );
   }
